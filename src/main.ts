441474e0d03bb176a62182fd6dde0856deda3ef6
riverdev <yariv.rivercybe@gmail.com>
2022-05-20 16:47:38 +0300

    First commit test

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
+
+//2022-05-23-1647 First commit 
