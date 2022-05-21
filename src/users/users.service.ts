6ab01dd8774fa15489f11ae443bbaf7909deaf7b
riverdev <yariv.rivercybe@gmail.com>
2022-05-21 18:49:36 +0300

    In users.service add function 'create' and wired it up to the 'signup' route in controller

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ReplaySubject } from 'rxjs';
+import { ReportsService } from 'src/reports/reports.service';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectRepository(User) private repo: Repository<User>){}
+
+    //The 'create' function creates a new user entry in the db
+    create(email: string, password: string){
+        const user = this.repo.create({email, password});
+        return this.repo.save(user);
+    }
+
+}
