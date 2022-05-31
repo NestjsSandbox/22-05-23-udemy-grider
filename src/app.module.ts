8df368e4b3c854cec0b170bb680333276c437c4a
riverdev <yariv.rivercybe@gmail.com>
2022-05-31 08:26:14 +0300

    2022-05-31-0824: Yariv: Added a new env var named COOKIE_KEY to .env.xx files and used that new env var inside AppModule to set the cookieSession value.

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,9 @@ import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/users.entity';
 import { Report } from './reports/report.entity';
-//import cookieSession from 'cookie-session';
-const cookieSession = require('cookie-session');
+const cookieSession = require('cookie-session'); //cannot use 'import' for cookie session
 
 @Module({
-
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
@@ -31,31 +29,31 @@ const cookieSession = require('cookie-session');
       }
     }),
 
-
-    // TypeOrmModule.forRoot({
-    //   type: 'sqlite',
-    //   database: 'db.sqlite',
-    //   entities: [User, Report], // Report entity added so that typeorm knows it exists
-    //   synchronize: true
-    // }),
     UsersModule,
-    ReportsModule],
+
+    ReportsModule], //end of 'imports' array value
+
   controllers: [AppController],
+
   providers: [
     AppService,
     {
       provide: APP_PIPE,
       useValue: new ValidationPipe({whitelist: true}),
     },
-  ], //end providers property
-})
-
+  ], //end 'providers' array value
+})//end of @Module decorator
 
 export class AppModule {
+
+//In the "@Module.imports" decorator.parameter above we setup the ConfigModule,
+// now here, inside the AppModule, we can use dependency injection to link and access it so that we can use it to supply us with the environement variables in the .env.xx files.
+  constructor( private configService: ConfigService){}
+
   configure( consumer: MiddlewareConsumer) {
     consumer
     .apply(
-      cookieSession({keys: ['blabla']})
+      cookieSession({keys: [this.configService.get('COOKIE_KEY')]})
     ).forRoutes('*');
   }
-}
+}//end of AppModule class
